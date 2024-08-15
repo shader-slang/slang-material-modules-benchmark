d31d50c1fc3d7281b5c1c8f5dc3d3912bd4c90dd
github-action-benchmark <github@users.noreply.github.com>
2024-08-15 00:03:46 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0ce943b953da3caa0ef488b5d5870a906fc83485

---
+++ dev/bench/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1723680226231,
+  "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vedavamadath@nvidia.com",
+            "name": "venkataram-nv",
+            "username": "venkataram-nv"
+          },
+          "committer": {
+            "email": "vedavamadath@nvidia.com",
+            "name": "venkataram-nv",
+            "username": "venkataram-nv"
+          },
+          "distinct": true,
+          "id": "0ce943b953da3caa0ef488b5d5870a906fc83485",
+          "message": "Workflow to update the github pages",
+          "timestamp": "2024-08-14T17:03:26-07:00",
+          "tree_id": "025fb702b32ea1d1f91196ec459c8d83d5944bef",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/0ce943b953da3caa0ef488b5d5870a906fc83485"
+        },
+        "date": 1723680225414,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : module : dxil",
+            "value": 3821,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 682,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 674,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 4399,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 1376,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 1413,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 3201,
+            "unit": "milliseconds"
+          },
+          {}
+        ]
+      }
+    ]
+  }
+}
