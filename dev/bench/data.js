2ae0d581b9896455cd59fab0c6676a478f1b5ee3
github-action-benchmark <github@users.noreply.github.com>
2024-08-15 00:10:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for bae6f6eccc81cbb174822882ed779d3d408cb44c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723680226231,
+  "lastUpdate": 1723680622608,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,66 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/0ce943b953da3caa0ef488b5d5870a906fc83485"
         },
         "date": 1723680225414,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : module : dxil",
+            "value": 3821,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 682,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 674,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 4399,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 1376,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 1413,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 3201,
+            "unit": "milliseconds"
+          },
+          {}
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vedavamadath@nvidia.com",
+            "name": "venkataram-nv",
+            "username": "venkataram-nv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bae6f6eccc81cbb174822882ed779d3d408cb44c",
+          "message": "Update README.md",
+          "timestamp": "2024-08-14T17:10:08-07:00",
+          "tree_id": "6674152a2da26e14916d360bc2e89aa1a4c63067",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/bae6f6eccc81cbb174822882ed779d3d408cb44c"
+        },
+        "date": 1723680622275,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
