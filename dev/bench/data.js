f63876daec04679cc64e6d7f0a2d524f424fc597
github-action-benchmark <github@users.noreply.github.com>
2024-10-31 06:52:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5654868311647eddda050467bf0f39386f099043

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730329873361,
+  "lastUpdate": 1730357571352,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -6045,6 +6045,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "precompilation : dxil",
             "value": 628.4375,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "committer": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "distinct": true,
+          "id": "5654868311647eddda050467bf0f39386f099043",
+          "message": "Constant-fold for the type-casting in switch-case labels (#5436)\n\nhttps://github.com/shader-slang/slang/commit/1487a1f98d8916985b243f4a0113d216c42e4ba3",
+          "timestamp": "2024-10-30T23:52:38-07:00",
+          "tree_id": "ab7a06f0b41191ac83c3c83945021492bf5c67c6",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/5654868311647eddda050467bf0f39386f099043"
+        },
+        "date": 1730357571018,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 1832.9375,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 510.25,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 511.625,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : module : dxil",
+            "value": 1829.25,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 503.625,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 505.125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 629.875,
             "unit": "milliseconds"
           }
         ]
