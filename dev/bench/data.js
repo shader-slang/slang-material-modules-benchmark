ccf47b49815e9a9f5abc8b04cc8d4c9ec5859e40
github-action-benchmark <github@users.noreply.github.com>
2024-11-23 00:23:44 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8afa4df20fb6735e7b8795c38f3530f7cb11fe57

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732319706405,
+  "lastUpdate": 1732321424746,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -9581,6 +9581,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "precompilation : dxil",
             "value": 625.5625,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "committer": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "distinct": true,
+          "id": "8afa4df20fb6735e7b8795c38f3530f7cb11fe57",
+          "message": "[AD] Add support for resolving custom derivatives where generic parameters can't be automatically inferred (#5630)\n\nhttps://github.com/shader-slang/slang/commit/9913cfbf68dab8c3c8c418dd28b71c2a65a55ae0",
+          "timestamp": "2024-11-22T16:23:34-08:00",
+          "tree_id": "de767c37a8d5c2a48d9a5f4bc8a0f938e087a809",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/8afa4df20fb6735e7b8795c38f3530f7cb11fe57"
+        },
+        "date": 1732321424438,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 1820.625,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 504.875,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 503.9375,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : module : dxil",
+            "value": 1809.4375,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 503.3125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 500.75,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 637.9375,
             "unit": "milliseconds"
           }
         ]
