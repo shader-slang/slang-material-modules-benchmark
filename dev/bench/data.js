c27c41bc18d5f1cd5ed6e9795a86de1a2c919b44
github-action-benchmark <github@users.noreply.github.com>
2025-04-01 20:29:42 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 966c466be153921f77f18916ecdf3a44d7ac5eaf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743512953824,
+  "lastUpdate": 1743539382334,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -26683,6 +26683,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "precompilation : dxil",
             "value": 570.1875,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "committer": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "distinct": true,
+          "id": "966c466be153921f77f18916ecdf3a44d7ac5eaf",
+          "message": "Use coopvec supporting dxcompiler.dll and dxil.dll (#6719)\n\nhttps://github.com/shader-slang/slang/commit/6f3b8351c9cb50d16573fbb204b98b6810e97d48",
+          "timestamp": "2025-04-01T13:29:30-07:00",
+          "tree_id": "a54c7ba9a9ca8d7a72b08e7cea97f50204ddee28",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/966c466be153921f77f18916ecdf3a44d7ac5eaf"
+        },
+        "date": 1743539380782,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 2079.25,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 467.3125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 470.8125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : module : dxil",
+            "value": 2068,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 468.125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 469.125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 568.4375,
             "unit": "milliseconds"
           }
         ]
