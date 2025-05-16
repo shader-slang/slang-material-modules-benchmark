cf79b661f2306c650a0d200a88a0cc68a98e0428
github-action-benchmark <github@users.noreply.github.com>
2025-05-16 15:46:08 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 29f0ee0a17d6988e51245fe53a6b3552d9672bfe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747355447224,
+  "lastUpdate": 1747410368624,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -33865,6 +33865,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "precompilation : dxil",
             "value": 646.5625,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "SLANGWIN4$@nvidia.com",
+            "name": "unknown"
+          },
+          "committer": {
+            "email": "SLANGWIN4$@nvidia.com",
+            "name": "unknown"
+          },
+          "distinct": true,
+          "id": "29f0ee0a17d6988e51245fe53a6b3552d9672bfe",
+          "message": "Fix RWStructuredBuffer emission (#7139)\n\nhttps://github.com/shader-slang/slang/commit/1fd7b2296d8360c245a0c732e7f842876533f92a",
+          "timestamp": "2025-05-16T08:37:18-07:00",
+          "tree_id": "59c156d6d5375fa8c03fa585c1e0fe55302cba0b",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/29f0ee0a17d6988e51245fe53a6b3552d9672bfe"
+        },
+        "date": 1747410366683,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 2060.75,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 485.1875,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 495.125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : module : dxil",
+            "value": 2037.125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 485.125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 484,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 647.125,
             "unit": "milliseconds"
           }
         ]
