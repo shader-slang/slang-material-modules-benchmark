a6112547caef4d3c1e478b45c6921e68a6133c17
github-action-benchmark <github@users.noreply.github.com>
2025-07-22 06:01:59 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 70dda30a2779b527319fe7f28c63d2cf61b8db67

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753155831409,
+  "lastUpdate": 1753164119148,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -46405,6 +46405,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "precompilation : dxil",
             "value": 970.621875,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "SLANGWIN4$@nvidia.com",
+            "name": "unknown"
+          },
+          "committer": {
+            "email": "SLANGWIN4$@nvidia.com",
+            "name": "unknown"
+          },
+          "distinct": true,
+          "id": "70dda30a2779b527319fe7f28c63d2cf61b8db67",
+          "message": "Fix Conditioanl<T, false> fields with a semantic. (#7855)\n\nhttps://github.com/shader-slang/slang/commit/9d47a352960efd71494c7dfa0918debd5b405077",
+          "timestamp": "2025-07-21T23:01:46-07:00",
+          "tree_id": "06c1d951870163707b70ff952b32725d8b09427f",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/70dda30a2779b527319fe7f28c63d2cf61b8db67"
+        },
+        "date": 1753164116604,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 2222.5099999999998,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 687.2125000000001,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 688.808125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : module : dxil",
+            "value": 2235.9087500000005,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 686.9087499999999,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 686.7256249999999,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 970.0099999999999,
             "unit": "milliseconds"
           }
         ]
