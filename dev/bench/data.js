35a6f3825a0d15774fd070726abdd0a4522a59fd
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 20:59:29 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3319f1ac5bbf947ef3bf9580c00624add3f3e74b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764619391007,
+  "lastUpdate": 1764622769310,
   "repoUrl": "https://github.com/shader-slang/slang-material-modules-benchmark",
   "entries": {
     "Benchmark": [
@@ -66514,6 +66514,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "precompilation : dxil",
             "value": 1145.708125,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "committer": {
+            "email": "SLANGWIN5$@nvidia.com",
+            "name": "unknown"
+          },
+          "distinct": true,
+          "id": "3319f1ac5bbf947ef3bf9580c00624add3f3e74b",
+          "message": "Update SPIRV for VulkanSDK Release Dec/2025 (#9180)\n\nhttps://github.com/shader-slang/slang/commit/8916cc6fb3dc98b807fb7dc934e4ac86d5c76a25",
+          "timestamp": "2025-12-01T12:59:16-08:00",
+          "tree_id": "7171284740145c0e9ceaac2266a1f0628f8ca6b8",
+          "url": "https://github.com/shader-slang/slang-material-modules-benchmark/commit/3319f1ac5bbf947ef3bf9580c00624add3f3e74b"
+        },
+        "date": 1764622765551,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "closesthit : mono : dxil",
+            "value": 2284.96875,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : mono : dxil",
+            "value": 745.5618749999999,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : mono : dxil",
+            "value": 748.358125,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "closesthit : module : dxil",
+            "value": 2266.044375,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "anyhit : module : dxil",
+            "value": 741.4437499999998,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "shadow : module : dxil",
+            "value": 743.65,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "precompilation : dxil",
+            "value": 1129.0193749999999,
             "unit": "milliseconds"
           }
         ]
